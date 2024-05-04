75d4a5ad66797c1a0ab8734e446f31a1424c11a5
Diedrich Vorberg <diedrich@tux4web.de>
2024-05-04 19:44:40 +0200

    Version incremented.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sqlclasses",
-    version="1.0",
+    version="1.2",
     author="Diedrich Vorberg",
     author_email="diedrich@tux4web.de",
     description="Construct SQL commands and queries using Python classes.",
@@ -19,7 +19,7 @@ setuptools.setup(
 
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v4 (GPLv4)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
